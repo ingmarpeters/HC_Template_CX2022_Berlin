19bfc0e3358d7c1c7681ae1ed40d3e338dae87ec
Ingmar Peters <65544475+ingmarpeters@users.noreply.github.com>
2022-04-21 14:23:48 +0200

    Widget

--- script.js
+++ script.js
@@ -486,3 +486,58 @@ document.addEventListener('click', function (event) {
     document.querySelector(event.target.dataset.target).style.display = 'none'
   }
 })
+
+//Include Sunshine Conversations Web SDK
+!function(o,p,s,e,c){
+  var i,a,h,u=[],d=[];function t(){var t="You must provide a supported major version.";try{if(!c)throw new Error(t);var e,n="https://cdn.smooch.io/",r="smooch";if((e="string"==typeof this.response?JSON.parse(this.response):this.response).url){var o=p.getElementsByTagName("script")[0],s=p.createElement("script");s.async=!0;var i=c.match(/([0-9]+)\.?([0-9]+)?\.?([0-9]+)?/),a=i&&i[1];if(i&&i[3])s.src=n+r+"."+c+".min.js";else{if(!(4<=a&&e["v"+a]))throw new Error(t);s.src=e["v"+a]}o.parentNode.insertBefore(s,o)}}catch(e){e.message===t&&console.error(e)}}o[s]={init:function(){i=arguments;var t={then:function(e){return d.push({type:"t",next:e}),t},catch:function(e){return d.push({type:"c",next:e}),t}};return t},on:function(){u.push(arguments)},render:function(){a=arguments},destroy:function(){h=arguments}},o.__onWebMessengerHostReady__=function(e){if(delete o.__onWebMessengerHostReady__,o[s]=e,i)for(var t=e.init.apply(e,i),n=0;n<d.length;n++){var r=d[n];t="t"===r.type?t.then(r.next):t.catch(r.next)}a&&e.render.apply(e,a),h&&e.destroy.apply(e,h);for(n=0;n<u.length;n++)e.on.apply(e,u[n])};var n=new XMLHttpRequest;n.addEventListener("load",t),n.open("GET","https://"+e+".webloader.smooch.io/",!0),n.responseType="json",n.send()
+}(window,document,"Smooch","625f3086c6f30400f3773442","5");
+
+//Initialise Sunshine Conversations Web Messenger
+Smooch.init({ integrationId: '625f3086c6f30400f3773442',
+canUserSeeConversationList: false,
+delegate: {
+       beforeDisplay(message, data) {
+       //Add avatarURL to message 
+       message.avatarUrl = "https://cdn.shopify.com/s/files/1/1061/1924/products/Robot_Emoji_Icon_7070a254-26f7-4a54-8131-560e38e34c2e_grande.png"
+       
+       //Do not display "hidden" messages
+       if (message.metadata && message.metadata.isHidden) {
+           return null;
+       }
+       return message;
+   }
+}
+}).then(function() {
+       // Your code after init is complete
+       //Create Conversation if one does not exist
+       var Conversations = Smooch.getConversations()
+       if ( Conversations.length == 0){
+           Smooch.createConversation({
+               displayName: "Welcome!!",
+               description: "Zendesk CX Trends 2022",
+           })
+       } 
+   }
+);
+
+//Send hidden message when the widget is opened and the conversation has no messages
+Smooch.on('widget:opened', function () {
+//Get Conversation & Send Message
+{
+           Smooch.getConversationById().then(function(conversation) {
+             if (conversation.messages.length == 0){
+               Smooch.sendMessage(
+               {
+                   type: 'text',
+                   text: 'DACH Roadshow',
+                   metadata: {
+                   isHidden:true
+                   }
+               },
+               conversation.id,
+               ); 
+               }
+             } 
+           );
+       }
+});
